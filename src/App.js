484de2312bde06fc9034e5be1991a6f8e540ca11
이혜원 <sunone2@hunet.co.kr>
2022-08-04 16:41:12 +0900

    응용추가

--- src/App.js
+++ src/App.js
@@ -1,103 +1,114 @@
+/* eslint-disable */
+
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
 function App() {
-  let menu = '강남 우동 맛집';
-  let [글제목,글제목변경] = useState(['남자 코드 추천','강남 우동 맛집','파이썬 독학']);
-  let [따봉,따봉변경] = useState([0,1,2]);
-  let [like1,likeAdd1] = useState(0);
-  let [like2,likeAdd2] = useState(0);
-  let [modal,setModal] = useState(false);
-  let [title,setTitle] =useState(0);
-  let [입력값,입력값변경] = useState('');
+	const now = new Date();	// 현재 날짜 및 시간
+	let month = now.getMonth();
+	let date = now.getDate();
 
-  
-  return (
-    <div className="App">
-      <div>
-        <h3 className="black-tit">블로그 </h3>
-        <button onClick={ () => {
-          let copy_title = [...글제목];
-          copy_title[0] = "여자 코트 추천";
-         // copy_title.sort();
-         글제목변경(copy_title);
-          } }>수정버튼</button>
-      </div>
-      
-      {/* <div className='list'>
-        <h4>{title[0]} <span onClick={ () => { likeAdd(like + 1) }}>👍</span> {like}</h4>
-        <p>7월 8일 발행</p>
-      </div>
-      <div className='list'>
-        <h4>{title[1]}</h4>
-        <p>7월 8일 발행</p>
-      </div>
-      <div className='list'>
-        <h4 onClick={() => {
-          setModal(!modal);
-          //modal == true ? setModal(false) : setModal(true)    
-          }}>{title[2]}</h4>
-        <p>7월 8일 발행</p>
-      </div> */}
+	let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','파이썬독학']);
+	let [따봉,따봉변경] = useState([0,0,0]);
+	let [modal,setModal] = useState(false);
+	let [title,setTitle] = useState(0);
+	let [입력값,입력값변경] = useState('');
+	let [날짜,날짜변경] = useState(['2월 17일','2월 17일','2월 17일']);
 
-      {
-        글제목.map(function(a,i){
-          return(
-            <div className='list' key={i}>
-              <h4> 
-                <span onClick={() => {setModal(!modal); setTitle(i)}}>{글제목[i]}</span> 
-                <span onClick={(e) => {e.stopPropagation(); 따봉변경(따봉[i] + 1,따봉[i] + 1) }}>👍 {따봉[i]}</span>
-              </h4>
-              <p>7월 8일 발행</p>
-              <button onClick={
-                () => {
-                  let copy = [...글제목];
-                  copy.splice(i,1);
-                  글제목변경(copy);
-                  }}>삭제</button>
-            </div>
-            
-          )
-        })
-      }
+	return (
+		<div className="App">
+			<div className="black-nav">
+				<h4>ReactBlog</h4>
+			</div>
+			<br></br>
+			<button onClick={()=>{
+				let copy =[...글제목];
+				글제목변경(copy.sort());
+			}}>가다나순정렬</button>
 
-      <input onChange={
-        (e)=>{
-          입력값변경(e.target.value);
-        }
-      }></input>
-      <button onClick={
-        ()=> {
-          console.log(입력값);
-          let copy = [...글제목];
-         copy.unshift(입력값);
-         글제목변경(copy);
-        }}>발행</button>
-      
-      {
-        (modal == true) ? <Modal title={title} 글제목={글제목} 글제목변경={글제목변경}></Modal> : null
-      }
-      
-    </div>   
-  );
+			<button onClick={()=>{ 
+				let copy =[...글제목];
+				copy[0] = '여자 코트 추천';
+				글제목변경(copy);
+			}}>글수정</button>
+
+			{
+				글제목.map(function(a,i){
+					return (
+						<div className="list" key={i}>
+							<h4 onClick={()=>{setModal(!modal);	setTitle(i);}}>
+								{ 글제목[i] } 
+								<span onClick={(e) => {
+									e.stopPropagation(); 
+									let copy =[...따봉];
+									copy[i] = copy[i]+1;
+									따봉변경(copy) 
+								}}>👍</span> 
+								{따봉[i]}
+							</h4>
+							<p>{날짜[i]} 발행</p>
+							<button onClick={()=>{
+								 let copy = [...글제목];
+								 copy.splice(i,1);
+								 글제목변경(copy);
+							}}>삭제</button>
+						</div>
+					)
+				})
+			}
+			
+			<input onChange={(e)=>{ 
+				입력값변경(e.target.value)
+			}} />
+			<button onClick={()=>{
+				입력값 != '' ? 실행() : '';
+			}}>글발행</button>
+			
+			{
+				modal == true ? <Modal 날짜={날짜} 글제목={글제목} 글제목변경={글제목변경} title={title} modal={modal} setModal={setModal}></Modal> : null
+			}	
+			
+		</div>
+	);
+
+	function 실행(){
+		let copy = [...글제목];
+		let copy1 = [...따봉];
+		let copy2 = [...날짜];
+		copy.unshift(입력값);
+		copy1.unshift(0);
+		copy2.unshift(month+1 + '월 ' + date +'일');
+		글제목변경(copy);
+		따봉변경(copy1);
+		날짜변경(copy2);
+	}
 }
 
+
+// 컴포넌트
 function Modal(props){
-  return(
-    <div className="modal">
-      <h4>{ props.글제목[props.title] }</h4>
-      <p>날짜</p>
-      <p>상세내용</p>
-      
-      <button onClick={ () => {
-        let copy_title = [...props.글제목];
-        copy_title[0] = "여자 코트 추천";
-        // copy_title.sort();
-        props.글제목변경(copy_title);
-        } }>수정버튼</button>
-    </div>
-  )
+	return (
+		<div className='modal-popup' onClick={()=>{
+			let copy = props.modal;
+			copy = false;
+			props.setModal(copy);
+			}}>
+			<div className="modal">
+				<h4>{props.글제목[props.title]}</h4>
+				<p>{props.날짜[props.title]}</p>
+				<p>상세내용</p>
+				<button onClick={()=>{ 
+					let copy =[...props.글제목];
+					copy[0] = '여자 코트 추천';
+					props.글제목변경(copy);
+				}}>글수정</button>
+			</div>
+		</div>
+	)
 }
+// const Modal = () => {
+// }
 
 export default App;
+
